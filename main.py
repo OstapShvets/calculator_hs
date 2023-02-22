fd7fe7a4f82eb8c2d3ffbc9acc5972bea25dae77
OstapShvets <shvetsostap200@gmail.com>
2023-02-22 18:45:56 +0200

    calculator

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ except ValueError:
     print("It must be only numbers")
     exit()
 
-value_operator = input("Please choose operator:\n 1 '+'\n 2 '-'\n 3 '*'\n 4 ''\n 5 '/'\nYour answer: ")
+value_operator = input("Please choose operator:\n 1 '+'\n 2 '-'\n 3 '*'\n 4 '**'\n 5 '/' ")
 
 if value_operator in ['1', '2', '3', '4', '5']:
     if value_operator == '1':
@@ -15,7 +15,7 @@ if value_operator in ['1', '2', '3', '4', '5']:
     elif value_operator == '3':
         result = value_float_1 * value_float_2
     elif value_operator == '4':
-        result = value_float_1 / value_float_2
+        result = value_float_1 ** value_float_2
     elif value_operator == "5":
         try:
             if value_float_2 == 0:
