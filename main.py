128898424a5a9bcd5324350612d454aeffcbb657
OstapShvets <shvetsostap200@gmail.com>
2023-02-23 16:50:14 +0200

    оновив пітон з помилками

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 try:
-    value_float_1 = float(input("Please type a number: "))
-    value_float_2 = float(input("Please type another number: "))
+    num1 = float(input("Please,type a number:"))
+    num2 = float(input("Type another number:"))
 except ValueError:
-    print("It must be only numbers")
+    print("It must be only numbers!")
     exit()
 
-value_operator = input("Please choose operator:\n 1 '+'\n 2 '-'\n 3 '*'\n 4 '**'\n 5 '/' ")
+value_operator = input("Please, choose your option: \n1 '+' \n2 '-' \n3 '*' \n4 '**' \n5 '/'")
 
 if value_operator in ['1', '2', '3', '4', '5']:
+    result = None
     if value_operator == '1':
-        result = value_float_1 + value_float_2
+        result = num1 + num2
     elif value_operator == '2':
-        result = value_float_1 - value_float_2
+        result = num1 - num2
     elif value_operator == '3':
-        result = value_float_1 * value_float_2
+        result = num1 * num2
     elif value_operator == '4':
-        result = value_float_1 ** value_float_2
-    elif value_operator == "5":
+       try:
+
+           if result > 20000:
+             print("Result too large")
+           else:
+               result = num1 ** num2
+       except NameError:
+           print("Result too large")
+       except TypeError:
+           print("Error")
+    elif value_operator == '5':
         try:
-            if value_float_2 == 0:
-                print("It can not be 0")
+            if num2 == 0:
+                print("It can be 0!")
             else:
-                result = value_float_1 / value_float_2
+                result = num1 / num2
+                print(result)
         except:
             print("Error")
 else:
-    print("It can be only 1,2,3,4 and 5")
-    result = None
-
-if result is not None:
-    print(result)
+    print("Only numbers from 1 to 5")
+print(result)
